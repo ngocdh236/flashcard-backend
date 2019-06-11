220b0034ef46649d6a06d90836f852aa4d1bd627
Hồng Ngọc Doãn <hongngocdoan@Nhims-MBP.home>
2019-06-11 11:13:35 +0300

    Changed access control origin

--- src/server.js
+++ src/server.js
@@ -16,7 +16,7 @@ mongoose.set('useFindAndModify', false)
 const app = express()
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
+  res.header('Access-Control-Allow-Origin', '*')
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
