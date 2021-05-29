1e4755aa586d47a9d69ca38a4fa7d5de056f6988
Ngoc <ngocdh236@gmail.com>
2021-05-29 20:06:24 +0300

    Endpoint to get categories decks

--- src/controllers/category.controller.js
+++ src/controllers/category.controller.js
@@ -1,7 +1,7 @@
-const { Category } = require('../models/Category');
-const { Deck } = require('../models/Deck');
-const { ACL } = require('../models/ACL');
-const { ObjectTitles, Rights } = require('../middlewares/checkAccessRight');
+const { Category } = require("../models/Category");
+const { Deck } = require("../models/Deck");
+const { ACL } = require("../models/ACL");
+const { ObjectTitles, Rights } = require("../middlewares/checkAccessRight");
 
 const create = (req, res) => {
   let errors = {};
@@ -9,43 +9,51 @@ const create = (req, res) => {
   const userId = req.user.id;
   Category.findOne({
     name,
-    userId
-  }).then(category => {
+    userId,
+  }).then((category) => {
     if (category) {
-      errors.name = 'Category already exists';
+      errors.name = "Category already exists";
       return res.status(400).json(errors);
     }
     Category.create({
       name,
-      userId
+      userId,
     })
-      .then(async category => {
+      .then(async (category) => {
         await ACL.create({
           objectTitle: ObjectTitles.CATEGORY,
           objectId: category.id,
           userId,
-          rights: [Rights.GET, Rights.UPDATE, Rights.REMOVE]
+          rights: [Rights.GET, Rights.UPDATE, Rights.REMOVE],
         });
         return res.status(201).json(category);
       })
-      .catch(err => res.status(400).json(err));
+      .catch((err) => res.status(400).json(err));
   });
 };
 
-const getAll = (req, res) => {
+const getUserCategories = (req, res) => {
   Category.find({ userId: req.user.id })
-    .then(categories => {
+    .then((categories) => {
       res.status(200).json(categories);
     })
-    .catch(err => res.status(400).json(err));
+    .catch((err) => res.status(400).json(err));
+};
+
+const getAllDecks = (req, res) => {
+  Deck.find({ categoryId: req.params.id })
+    .then((decks) => {
+      res.status(200).json(decks);
+    })
+    .catch((err) => res.status(400).json(err));
 };
 
 const getById = (req, res) => {
   Category.findById(req.params.id)
-    .then(category => {
+    .then((category) => {
       res.status(200).json(category);
     })
-    .catch(err => res.status(400).json(err));
+    .catch((err) => res.status(400).json(err));
 };
 
 const update = (req, res) => {
@@ -54,26 +62,33 @@ const update = (req, res) => {
     .then(
       res
         .status(200)
-        .json({ success: true, message: 'Category updated successfully' })
+        .json({ success: true, message: "Category updated successfully" })
     )
-    .catch(err => res.status(400).json(err));
+    .catch((err) => res.status(400).json(err));
 };
 
 const remove = async (req, res) => {
   const { id } = req.params;
 
   Category.findByIdAndDelete(id)
-    .then(async response => {
+    .then(async (response) => {
       await Deck.updateMany({ categoryId: category.id }, { categoryId: null });
       await ACL.deleteMany({
         objectTitle: ObjectTitles.CATEGORY,
-        objectId: id
+        objectId: id,
       });
       res
         .status(200)
-        .json({ success: true, message: 'Category removed successfully' });
+        .json({ success: true, message: "Category removed successfully" });
     })
-    .catch(err => res.status(400).json(err));
+    .catch((err) => res.status(400).json(err));
 };
 
-exports.CategoryController = { create, getAll, getById, update, remove };
+exports.CategoryController = {
+  create,
+  getUserCategories,
+  getAllDecks,
+  getById,
+  update,
+  remove,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,15 @@
-const express = require('express')
-const passport = require('passport')
+const express = require("express");
+const passport = require("passport");
 
-const { UserController } = require('../controllers/user.controller')
-const { CategoryController } = require('../controllers/category.controller')
-const { DeckController } = require('../controllers/deck.controller')
+const { UserController } = require("../controllers/user.controller");
+const { CategoryController } = require("../controllers/category.controller");
+const { DeckController } = require("../controllers/deck.controller");
 
 const {
   checkAccessRight,
   ObjectTitles,
-  Rights
-} = require('../middlewares/checkAccessRight')
+  Rights,
+} = require("../middlewares/checkAccessRight");
 const {
   validateInputModel,
   Register,
@@ -18,54 +18,62 @@ const {
   ChangeUserPassword,
   Category,
   Deck,
-  Card
-} = require('../middlewares/validateInput')
+  Card,
+} = require("../middlewares/validateInput");
 
-const router = express.Router()
-const passportJwt = passport.authenticate('jwt', { session: false })
+const router = express.Router();
+const passportJwt = passport.authenticate("jwt", { session: false });
 
 // USERS
-const usersUrl = '/users'
+const usersUrl = "/users";
 
 router.post(
   `${usersUrl}/register`,
   validateInputModel(Register),
   UserController.register
-)
+);
 
 router.post(
   `${usersUrl}/login`,
   validateInputModel(Login),
   UserController.login
-)
+);
 
 router.put(
   usersUrl,
   passportJwt,
   validateInputModel(UpdateUser),
   UserController.update
-)
+);
 
 router.patch(
   `${usersUrl}/password`,
   passportJwt,
   validateInputModel(ChangeUserPassword),
   UserController.changePassword
-)
-router.delete(usersUrl, passportJwt, UserController.remove)
+);
+router.delete(usersUrl, passportJwt, UserController.remove);
 
 // CATEGORIES
-const categoriesUrl = '/categories'
+const categoriesUrl = "/categories";
 
 router.post(
   categoriesUrl,
   passportJwt,
   validateInputModel(Category),
   CategoryController.create
-)
+);
 
-// Get all
-router.get(categoriesUrl, passportJwt, CategoryController.getAll)
+// Get user categories
+router.get(categoriesUrl, passportJwt, CategoryController.getUserCategories);
+
+// Get all decks
+router.get(
+  `${categoriesUrl}/:id/decks`,
+  passportJwt,
+  checkAccessRight(ObjectTitles.CATEGORY, Rights.GET),
+  CategoryController.getAllDecks
+);
 
 // Get by id
 router.get(
@@ -73,7 +81,7 @@ router.get(
   passportJwt,
   checkAccessRight(ObjectTitles.CATEGORY, Rights.GET),
   CategoryController.getById
-)
+);
 
 router.put(
   categoriesUrl,
@@ -81,27 +89,27 @@ router.put(
   checkAccessRight(ObjectTitles.CATEGORY, Rights.UPDATE),
   validateInputModel(Category),
   CategoryController.update
-)
+);
 
 router.delete(
   `${categoriesUrl}/:id`,
   passportJwt,
   checkAccessRight(ObjectTitles.CATEGORY, Rights.REMOVE),
   CategoryController.remove
-)
+);
 
 // DECKS
-const decksUrl = '/decks'
+const decksUrl = "/decks";
 
 router.post(
   decksUrl,
   passportJwt,
   validateInputModel(Deck),
   DeckController.create
-)
+);
 
 // Get all
-router.get(decksUrl, passportJwt, DeckController.getAll)
+router.get(decksUrl, passportJwt, DeckController.getAll);
 
 // Get by id
 router.get(
@@ -109,7 +117,7 @@ router.get(
   passportJwt,
   checkAccessRight(ObjectTitles.DECK, Rights.GET),
   DeckController.getById
-)
+);
 
 router.put(
   decksUrl,
@@ -117,13 +125,13 @@ router.put(
   checkAccessRight(ObjectTitles.DECK, Rights.UPDATE),
   validateInputModel(Deck),
   DeckController.update
-)
+);
 
 router.delete(
   `${decksUrl}/:id`,
   passportJwt,
   checkAccessRight(ObjectTitles.DECK, Rights.REMOVE),
   DeckController.remove
-)
+);
 
-exports.router = router
+exports.router = router;
